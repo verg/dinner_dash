f71d8fcb1144f1a1e8f0f84fe5beeffde7c33932
verg <ryanvergeront@gmail.com>
2014-01-08 18:17:19 -0500

    initialize with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DinnerDash::Application.config.secret_key_base = '6b6da1df9668b7432f0ea950cb442db97523a4490a03c878b58f2913279683daa0f7549970981450648171f5305f2ac9a8dd5bd6e82a11763b82d022062c0142'
---
+++ spec/integration/browse_items_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+feature "Items" do
+  scenario "Unauthenticated user browses all items" do
+  end
+end
