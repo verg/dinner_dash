6b7d51b56251573ad3df869a7bf46904e585208f
verg <ryanvergeront@gmail.com>
2014-01-15 12:54:52 -0500

    Display associated items on Category#show page

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -8,6 +8,15 @@ describe CategoriesController do
       expect(assigns(:category)).to eq category
     end
 
+    it 'assigns the items associated with the category to @items' do
+      category = create(:category)
+      in_category = category.items.create(title: "Pizza", price_cents: 150 )
+      not_in_category = Item.create(title: "Not Pizza", price_cents: 150 )
+
+      get :show, id: category
+      expect(assigns(:items)).to eq [in_category]
+    end
+
     it "renders the :show template" do
       category = create(:category)
       get :show, id: category
--- spec/integration/browse_items_spec.rb
+++ spec/integration/browse_items_spec.rb
@@ -3,12 +3,22 @@ require 'spec_helper'
 feature "Items" do
   describe "Unauthenticated users" do
     scenario "browses all items" do
-      mapo = create(:item, title: "Mapo Tofu")
+      create(:item, title: "Mapo Tofu", price_cents: 899)
       visit root_path
       expect(page).to have_css ".item", text: "Mapo Tofu"
     end
 
-    scenario "browse items by category"
+    scenario "browse items by category" do
+      entrees = create(:category, name: "entrees")
+      entrees.items.create(title: "Mapo Tofu", price_cents: 899)
+      drinks = create(:category, name: "drinks")
+      drinks.items.create(title: "Jasmine Tea", price_cents: 250)
+
+      visit root_path
+      click_link("Entrees")
+      expect(page).to have_css ".item", text: "Mapo Tofu"
+      expect(page).not_to have_css ".item", text: "Jasmine Tea"
+    end
 
     scenario "Add items to cart"
     scenario "View cart"
