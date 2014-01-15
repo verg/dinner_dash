d561abb658327217eab7c1b1d48f9f93e6bebd39
verg <ryanvergeront@gmail.com>
2014-01-15 15:17:43 -0500

    Add Cart model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140115195959) do
+ActiveRecord::Schema.define(version: 20140115200948) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -36,9 +36,6 @@ ActiveRecord::Schema.define(version: 20140115195959) do
     t.string   "photo"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "category_id"
   end
-
-  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
 end
