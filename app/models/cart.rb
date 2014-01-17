c06074b51ac48479a4ac2d8ce108d9b46970d19d
verg <ryanvergeront@gmail.com>
2014-01-17 13:57:36 -0500

    Add User has_one Cart relationship

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ActiveRecord::Base
   has_many :line_items
+  belongs_to :user
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Cart do
   it { should have_many(:line_items) }
+  it { should belong_to(:user) }
 end
