14cffde5e6bf73a73cca1690c8216bfe925b21c1
verg <ryanvergeront@gmail.com>
2014-01-17 17:07:47 -0500

    Cart#add_product

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ActiveRecord::Base
   has_many :line_items
   belongs_to :user
+
+  def add_product(product_id, quantity=1)
+    line_items.create(product: product_id, quantity: quantity)
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,4 +3,13 @@ require 'spec_helper'
 describe Cart do
   it { should have_many(:line_items) }
   it { should belong_to(:user) }
+
+  describe ".add_product" do
+    it "adds a line item to the cart for the product" do
+      cart = Cart.create
+      product = create(:product)
+      cart.add_product(product)
+      expect(cart.line_items.first.product_id).to eq product.id
+    end
+  end
 end
