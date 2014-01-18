2d2b38434bffbf6de38723e93c1887602258c74e
verg <ryanvergeront@gmail.com>
2014-01-18 13:34:54 -0500

    User has_many LineItems

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
   has_one :cart
+  has_many :line_items, through: :cart
 
   def items_in_cart
-    c = cart || build_cart
-    c.line_items
+    line_items
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe User do
   it { should have_one(:cart) }
+  it { should have_many(:line_items) }
 
   describe ".items_in_cart" do
     it "returns line items in a user's cart" do
-      user = User.create
-      in_cart = create(:line_item, cart: user.build_cart)
+      user = create(:user)
+      in_cart = create(:line_item, cart: user.create_cart)
       not_in_cart = create(:line_item)
+
       expect(user.items_in_cart).to eq [in_cart]
     end
   end
