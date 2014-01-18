d8240d7c46f1cf70e12cadc7f9e7ecf804502764
verg <ryanvergeront@gmail.com>
2014-01-17 19:05:13 -0500

    Add LineItem#increment_quantity

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,10 @@
 class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
+
+  def increment_quantity(by=1)
+    original_qty = "CASE WHEN quantity IS NULL THEN 0 ELSE quantity END"
+    self.class.update_all("quantity = quantity + #{by.to_i}", "id = #{id}")
+    reload
+  end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,4 +3,13 @@ require 'spec_helper'
 describe LineItem do
   it { should belong_to(:cart) }
   it { should belong_to(:product) }
+
+  describe "#increment_quantity" do
+    it 'increments the quanity' do
+      line_item = create(:line_item)
+      quantity = line_item.quantity
+      line_item.increment_quantity
+      expect(line_item.quantity).to eq quantity + 1
+    end
+  end
 end
