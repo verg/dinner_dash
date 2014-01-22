f4ad2c9e594c09369c7626789139506b48873d15
verg <ryanvergeront@gmail.com>
2014-01-22 16:21:56 -0500

    Prevent n+1: Eager load products in CartsController#show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
   def show
-    @cart = current_cart
+    @cart = Cart.includes(line_items: [:product]).find_by(user_id: current_or_guest_user.id)
   end
 end
