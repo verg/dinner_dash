c4dfa9dc6af3b8d0c34283da5850c27e9371f7da
verg <ryanvergeront@gmail.com>
2014-01-28 18:34:16 -0500

    Migration: Add stripe_customer_id to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125183451) do
+ActiveRecord::Schema.define(version: 20140128224715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,20 +46,19 @@ ActiveRecord::Schema.define(version: 20140125183451) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "product_id"
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
   create_table "orders", force: true do |t|
-    t.integer  "line_item_id"
     t.integer  "user_id"
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "orders", ["line_item_id"], name: "index_orders_on_line_item_id", using: :btree
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "products", force: true do |t|
@@ -87,6 +86,7 @@ ActiveRecord::Schema.define(version: 20140125183451) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.string   "stripe_customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,19 @@
 #
 #   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create!(name: 'Emanuel', city: cities.first)
-[Category, Product, LineItem].each { |model| model.destroy_all }
+unless Rails.env.production?
+  [Category, Product, LineItem].each { |model| model.destroy_all }
 
-entrees = Category.create!(name: "Entrees")
-drinks = Category.create!(name: "Drinks")
-soups = Category.create!(name: "Soups")
-appetizers = Category.create!(name: "Appetizers")
+  entrees = Category.create!(name: "Entrees")
+  drinks = Category.create!(name: "Drinks")
+  soups = Category.create!(name: "Soups")
+  appetizers = Category.create!(name: "Appetizers")
 
-entrees.products.create!(title: "Mapo Tofu", price_cents: 899, display_rank: 3)
-drinks.products.create!(title: "Jasmine Tea", price_cents: 150, display_rank: 6)
-appetizers.products.create!(title: "Dan Dan Noodles", price_cents: 499, display_rank: 5)
+  entrees.products.create!(title: "Mapo Tofu", price_cents: 899, display_rank: 3)
+  drinks.products.create!(title: "Jasmine Tea", price_cents: 150, display_rank: 6)
+  appetizers.products.create!(title: "Dan Dan Noodles", price_cents: 499, display_rank: 5)
 
-hot_pot = Product.create!(title: "Hot Pot", price_cents: 1699, display_rank: 3)
-entrees.products << hot_pot
-soups.products << hot_pot
+  hot_pot = Product.create!(title: "Hot Pot", price_cents: 1699, display_rank: 3)
+  entrees.products << hot_pot
+  soups.products << hot_pot
+end
