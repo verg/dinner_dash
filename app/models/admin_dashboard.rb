3b3160d0142ad973babda8a4100e73c88c209249
verg <ryanvergeront@gmail.com>
2014-01-31 14:55:25 -0500

    Add attr_reader :categories to AdminDashboard

--- app/models/admin_dashboard.rb
+++ app/models/admin_dashboard.rb
@@ -1,3 +1,7 @@
 class AdminDashboard
+  attr_reader :categories
 
+  def initialize
+    @categories = Category.all
+  end
 end
--- spec/models/admin_dashboard_spec.rb
+++ spec/models/admin_dashboard_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe AdminDashboard do
-
+  it "gives access to all Categories" do
+    category = create(:category)
+    dashboard = AdminDashboard.new
+    expect(dashboard.categories).to match_array([category])
+  end
 end
