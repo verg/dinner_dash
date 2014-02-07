564ad54e9a5a489d74448fe22cf3ab9919e945a9
verg <ryanvergeront@gmail.com>
2014-02-07 09:32:27 -0500

    Redesign OrderSearch

--- app/models/order_search.rb
+++ app/models/order_search.rb
@@ -1,6 +1,31 @@
 class OrderSearch
-  def self.by_user_attributes search_term
-    users = User.includes(:orders).basic_search(search_term).references(:orders)
-    users.reduce([]) { |orders, user| orders + user.orders }
+
+  def search(args)
+    orders_page = args.fetch(:orders_page) { 1 }
+    orders_per_page = args.fetch(:orders_per_page) { 30 }
+    order_by = args.fetch(:order_by_query) { "created_at DESC" }
+
+    search_by_user_query = args.fetch(:search_by_user_query) { nil }
+    order_status = args.fetch(:order_status) { nil }
+
+    Order.where(query_conditions(search_by_user_query, order_status)).
+      order(order_by).paginate(page: orders_page, per_page: orders_per_page)
+  end
+
+  private
+
+  def query_conditions(search_term, status)
+    args = {}
+    args = args.merge(user_id_args_for(search_term)) if search_term
+    args = args.merge(status_query_args(status)) if status
+    args
+  end
+
+  def user_id_args_for(search_term)
+    { user_id:  User.basic_search(search_term).map(&:id) }
+  end
+
+  def status_query_args(status)
+    Order.status_query_args(status)
   end
 end
--- spec/models/order_search_spec.rb
+++ spec/models/order_search_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe OrderSearch do
-  describe ".by_user_attributes" do
+  let(:searcher) { OrderSearch.new }
+
+  describe "#search" do
     it "returns orders for all Users matching the search term on any user attribute" do
       bob = create(:user, firstname: "Bob", lastname: "James", email: "funguy@gmail.com")
       alice = create(:user, firstname: "Alice", lastname: "James", email: "alice@gmail.com")
@@ -11,17 +13,40 @@ describe OrderSearch do
       alice_order = create(:order, user: alice)
       other_order = create(:order, user: other_guy)
 
-      lastname_results = OrderSearch.by_user_attributes("James")
+      lastname_results = searcher.search(search_by_user_query: "James")
       expect(lastname_results).to include(alice_order, bob_order)
       expect(lastname_results).not_to include(other_order)
 
-      firstname_results = OrderSearch.by_user_attributes("Bob")
+      firstname_results = searcher.search(search_by_user_query: "Bob")
       expect(firstname_results).to include(bob_order)
       expect(firstname_results).not_to include(alice_order, other_order)
 
-      email_results = OrderSearch.by_user_attributes("funguy@gmail.com")
+      email_results = searcher.search(search_by_user_query: "funguy@gmail.com")
       expect(email_results).to include(bob_order)
       expect(email_results).not_to include(alice_order, other_order)
+    end
+
+    it "filters by status" do
+      complete_order = create(:order, complete: true)
+      paid_order = create(:order, paid: true)
+
+      lastname_results = searcher.search(order_status: :paid )
+      expect(lastname_results).to include(paid_order)
+      expect(lastname_results).not_to include(complete_order)
+    end
+
+    it "filters by both status and a search term" do
+      bob = create(:user, firstname: "Bob", lastname: "James", email: "funguy@gmail.com")
+      alice = create(:user, firstname: "Alice", lastname: "James", email: "alice@gmail.com")
+      other_guy = create(:user, firstname: "Other", lastname: "Guy", email: "guy@yahoo.com")
+
+      complete_order = create(:order, user: bob, complete: true)
+      paid_order = create(:order, user: alice, paid: true)
+      other_order = create(:order, user: other_guy)
+
+      lastname_results = searcher.search(search_by_user_query: "James", order_status: :paid )
+      expect(lastname_results).to include(paid_order)
+      expect(lastname_results).not_to include(other_order, complete_order)
     end
   end
 end
